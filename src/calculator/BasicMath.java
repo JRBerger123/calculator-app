35f31f1622dadb7784f517113d8a0feda58e84d7
JRBerger123 <129473627+JRBerger123@users.noreply.github.com>
2025-04-07 13:22:45 -0500

    Added JavaDocs to Calculator.java. Fixed return types for Calculator class and the BasicMath interface. Implemented updateDisplay method.

--- src/calculator/BasicMath.java
+++ src/calculator/BasicMath.java
@@ -1,8 +1,8 @@
 package calculator;
 
 public interface BasicMath <N extends Number> {
-    N add(N a, N b);
-    N subtract(N a, N b);
-    N multiply(N a, N b);
-    N divide(N a, N b);
+    void add(N a);
+    void subtract(N a);
+    void multiply(N a);
+    void divide(N a);
 }
--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,27 +1,110 @@
 package calculator;
 
-import java.util.Scanner;
-
+/**
+ * The {@code Calculator} class represents a calculator object. <p>
+ * The {@code Calculator} class implements the {@code BasicMath} interface and defines the following methods:
+ * <ul>
+ *      <li>void add(N)</li>
+ *      <li>void subtract(N)</li>
+ *      <li>void multiply(N)</li>
+ *      <li>void divide(N)</li>
+ * </ul>
+ * Methods available:
+ * <ul>
+ *      <li>{@link #add(N)} - Adds two variables.</li>
+ *      <li>{@link #subtract(N)} - Subtracts two variables.</li>
+ *      <li>{@link #multiply(N)} - Multiplies two variables.</li>
+ *      <li>{@link #divide(N)} - Divides two variables.</li>
+ *      <li>{@link #clear()} - Clears class attributes.</li>
+ *      <li>{@link #updateDisplay()} - Displays what mathematical operation happened and the results.</li>
+ * </ul>
+ *
+ * @author Brandon Berger, ...
+ * @version 0.1
+ * @since 2025.04.07
+ * @see <a href="https://github.com/JRBerger123/Calculator-App">GitHub Repository</a>
+ * @see <a href="https://github.com/JRBerger123">Brandon Berger's GitHub</a>
+ */
 public class Calculator<N extends Number> implements BasicMath<N> {
-    double previosValue = 0.0;
+    /**
+     * Represents the previous value of the calculator.
+     */
+    double previousValue = 0.0;
+
+    /**
+     * Represents the current value of the calculator.
+     * This value is updated after each operation.
+     */
     double currentValue = 0.0;
+
+    /**
+     * Represents the new input value of the calculator.
+     * This value is used for calculations.
+     */
     double inputValue = 0.0;
+
+    /**
+     * Represents the operator used for the last operation.
+     */
     char operator = '+';
 
-    public void multiply(N a, N b) {
-        return a.doubleValue() * b.doubleValue();
+    /**
+     * Adds the input value to the previous value.
+     * @param a the input value to be added
+     */
+    public void add(N a) {
+        this.operator = '+';
+        this.currentValue = this.previousValue + a.doubleValue();
+        updateDisplay();
     }
 
-    public void Clear() {
-        previosValue = 0.0;
+    /**
+     * Subtracts the input value from the previous value.
+     * @param a the input value to be subtracted
+     */
+    public void subtract(N a) {
+        this.operator = '-';
+        this.currentValue = this.previousValue - a.doubleValue();
+    }
+
+    /**
+     * Multiplies the input value with the previous value.
+     * @param a the input value to be multiplied
+     */
+    public void multiply(N a) {
+        this.operator = '*';
+        this.currentValue = this.previousValue * a.doubleValue();
+    }
+
+    /**
+     * Divides the previous value by the input value.
+     * @param a the input value to divide by
+     */
+    public void divide(N a) {
+        if (a.doubleValue() == 0) {
+            System.out.println("Error: Division by zero is not allowed.");
+            return;
+        }
+
+        this.operator = '/';
+        this.currentValue = this.previousValue / a.doubleValue();
+    }
+
+    /**
+     * Clears the calculator's previous and current values, input value, and operator.
+     */
+    public void clear() {
+        previousValue = 0.0;
         currentValue = 0.0;
         inputValue = 0.0;
         operator = '+';
     }
 
-    public void subtract(N a, N b) {
-        return a.doubleValue() - b.doubleValue();
+    /**
+     * Updates the display with the current operation and result.
+     * This method is called after each operation to show the result.
+     */
+    public void updateDisplay() {
+        System.out.println(previousValue + " " + operator + " " + inputValue + " = " + currentValue);
     }
-
-
 }
